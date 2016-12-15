9794a92ab46024be054c93eac415e6959d113278
maxim.s <maxim.shayakhmetov@digitalalchemy.com.au>
2016-12-15 16:22:02 +1100

    modified output

--- facts.d/check_drbd_master.sh
+++ facts.d/check_drbd_master.sh
@@ -3,7 +3,7 @@
 drbd_status=`/sbin/drbdsetup status |sed -n '1 p'|cut -d : -f2`
 if [ $drbd_status ];then
   if [ $drbd_status=='Primary' ] || [ $drbd_status=='Secondary' ]; then
-    echo $drbd_status;
+    echo "drbd_node_status=$drbd_status";
   fi
   else
     echo 'unknown DRBD status, please verify DRBD service is functioning properly';
