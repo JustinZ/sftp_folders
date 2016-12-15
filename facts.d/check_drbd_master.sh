d2806e4be55feac718069b157f86eff0e8ce851a
maxim.s <maxim.shayakhmetov@digitalalchemy.com.au>
2016-12-15 12:37:30 +1100

    added external fact

---
+++ facts.d/check_drbd_master.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+drbd_status=`/sbin/drbdsetup status |sed -n '1 p'|cut -d : -f2`
+if [ $drbd_status ];then
+  if [ $drbd_status == 'Primary' ] || [ $drbd_status =='Secondary' ]; then
+    echo $drbd_status;
+  fi
+  else
+    echo 'unknown DRBD status, please verify DRBD service is functioning properly'
+fi
