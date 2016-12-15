cd2a4ecbd450daccab688e75e7c8c152adae3990
maxim.s <maxim.shayakhmetov@digitalalchemy.com.au>
2016-12-15 12:58:50 +1100

    added retrun statement

--- facts.d/check_drbd_master.sh
+++ facts.d/check_drbd_master.sh
@@ -3,7 +3,9 @@ drbd_status=`/sbin/drbdsetup status |sed -n '1 p'|cut -d : -f2`
 if [ $drbd_status ];then
   if [ $drbd_status == 'Primary' ] || [ $drbd_status =='Secondary' ]; then
     echo $drbd_status;
+    return 0;
   fi
   else
-    echo 'unknown DRBD status, please verify DRBD service is functioning properly'
+    echo 'unknown DRBD status, please verify DRBD service is functioning properly';
+    return 0;
 fi
