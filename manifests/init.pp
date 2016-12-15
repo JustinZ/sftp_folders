d2b8ba80a5587978addeacd14bda91e579508179
maxim.s <maxim.shayakhmetov@digitalalchemy.com.au>
2016-12-15 13:42:12 +1100

    typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,7 +20,7 @@ class sftp_folders(
     $mode = '0650',
 
   ) {
-notify { "Node DRBD status is $::check_drbd_master": }  
+notify { $::check_drbd_master: }  
 keys($dir_list).each | String $client_env |  
 {
   $dir_list[$client_env][directory].each|String $dir_name |
