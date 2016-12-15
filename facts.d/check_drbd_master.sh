71f40f173cc078fd1497aaaa945f3ede0efa5408
maxim.s <maxim.shayakhmetov@digitalalchemy.com.au>
2016-12-15 13:23:59 +1100

    typo

--- facts.d/check_drbd_master.sh
+++ facts.d/check_drbd_master.sh
@@ -2,7 +2,7 @@
 #script checks DRBD status on a host
 drbd_status=`/sbin/drbdsetup status |sed -n '1 p'|cut -d : -f2`
 if [ $drbd_status ];then
-  if [ $drbd_status == 'Primary' ] || [ $drbd_status =='Secondary' ]; then
+  if [[ $drbd_status == 'Primary' ] || [ $drbd_status =='Secondary' ]]; then
     echo $drbd_status;
     exit 0;
   fi
