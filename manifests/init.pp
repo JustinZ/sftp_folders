1eb59af375db7ddc93dc9cb84884305597bfdb50
maxim.s <maxim.shayakhmetov@digitalalchemy.com.au>
2017-05-10 11:53:58 +1000

    added notify

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,10 +36,10 @@ if $::drbd_node_status == 'Primary' {
 	 
 	  $parent = regsubst($dir_name, '/[^/]*/?$', '')
 	  if ($parent != $dir_name) and ($parent != '') {
+	    notify => {"$parent":},
 	    exec { "create parent directory $parent for $dir_name":
 	      # mode? uid/gid?  you decide...
 	      command => "/bin/mkdir -m 755 -p $parent",
-        notify {"$parent"}
 	      creates => "$parent",
 	      before => File[$dir_name],
 	    }
